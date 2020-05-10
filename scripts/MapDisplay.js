114c6564f5a15643c90fa8a9428b5c2484e40719
Moses <netshitanganimoses@gmail.com>
2020-05-10 23:33:26 +0200

    Default map now showing

--- scripts/MapDisplay.js
+++ scripts/MapDisplay.js
@@ -1,2 +1,19 @@
-// Initializing HERE maps api
+// Initializing HERE maps platform
 const appid = 'atv7oByWKH2AxK60DqTf';
+const apikey = 'NMv1ZzOrXhl_P9fbAuennDekhdoGt3EK6K5ZsPjiYPo';
+
+let platform = new H.service.Platform({
+    'apikey': 'NMv1ZzOrXhl_P9fbAuennDekhdoGt3EK6K5ZsPjiYPo'
+});
+
+// Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+    document.getElementById('mapContainer'),
+    defaultLayers.vector.normal.map,
+    {
+        zoom: 10,
+        center: { lat: -26.26, lng: 27.89 }
+    });
