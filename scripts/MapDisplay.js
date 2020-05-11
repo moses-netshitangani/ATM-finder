ba13684fb38c070d1f0482eca9189e0e54d200ce
Moses <netshitanganimoses@gmail.com>
2020-05-11 22:25:51 +0200

    now marks user's location on map

--- scripts/MapDisplay.js
+++ scripts/MapDisplay.js
@@ -2,18 +2,43 @@
 const appid = 'atv7oByWKH2AxK60DqTf';
 const apikey = 'NMv1ZzOrXhl_P9fbAuennDekhdoGt3EK6K5ZsPjiYPo';
 
+// fetch(`https://cors-anywhere.herokuapp.com/https://revgeocode.search.hereapi.com/v1/revgeocode?xnlp=CL_JSMv3.1.15.2&apikey=NMv1ZzOrXhl_P9fbAuennDekhdoGt3EK6K5ZsPjiYPo&at=-26.271%2C27.89%2C50`)
+// .then(result => result = result.json())
+// .then(res => console.log(res));
+
 let platform = new H.service.Platform({
-    'apikey': 'NMv1ZzOrXhl_P9fbAuennDekhdoGt3EK6K5ZsPjiYPo'
+    'useCIT': true,
+    'app_id': appid,
+    'apikey': 'NMv1ZzOrXhl_P9fbAuennDekhdoGt3EK6K5ZsPjiYPo',
+    'useHTTPS': true
 });
 
-// Obtain the default map types from the platform object:
-var defaultLayers = platform.createDefaultLayers();
+// Reverse geocoding service
+let service = platform.getSearchService();
+let lat, lon;
 
-// Instantiate (and display) a map object:
-var map = new H.Map(
-    document.getElementById('mapContainer'),
-    defaultLayers.vector.normal.map,
-    {
-        zoom: 10,
-        center: { lat: -26.26, lng: 27.89 }
-    });
+let render = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+        lati = position.coords.latitude;
+        lati = lati.toFixed(3);
+        lon = position.coords.longitude;
+        lon = lon.toFixed(3);
+
+        // Obtain the default map types from the platform object:
+        let defaultLayers = platform.createDefaultLayers();
+
+        // Instantiate (and display) a map object:
+        let map = new H.Map(
+            document.getElementById('mapContainer'),
+            defaultLayers.vector.normal.map,
+            {
+                zoom: 5,
+                center: { lat: lati, lng: 27.89 }
+            });
+        // Marking user's current location on map
+        map.addObject(new H.map.Marker({lat: lati, lng: lon}));
+
+    })
+}
+render();
+
